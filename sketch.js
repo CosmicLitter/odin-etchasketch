eba12c90e99809f7722ba923c4d4ec0dee8ccda6
CosmicLitter <holtbd91@gmail.com>
2023-08-23 16:30:27 -0400

    Initial structure and grid generator

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const sketch = document.querySelector('.sketch')
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = "This was added with Javascript";
+
+
+
+let canvasSize = 2;
+
+const canvasSelect = document.querySelector('#canvas');
+canvasSelect.addEventListener('change', setCanvas)
+
+function setCanvas() {
+    canvasSize = canvasSelect.value;
+    sketch.innerHTML = ''
+
+    for (let x = 0; x < canvasSize; x++) {
+        const column = document.createElement('div');
+        column.classList.add('column');
+        for (let i = 0; i < canvasSize; i++) {
+            const pixel = document.createElement('div')
+            pixel.classList.add('pixel');
+            column.append(pixel);
+        }
+
+        sketch.appendChild(column)
+    }
+
+}
+
+setCanvas()
+
